dbe437f66b02e92a8fbf07947d6db678152fae82
Nirdash <gadzyara12@gmail.com>
2018-02-14 14:04:31 +0300

    - Ксюшины изменения тута

--- src/main/java/com/praktos/praktos/repository/SourcesRepository.java
+++ src/main/java/com/praktos/praktos/repository/SourcesRepository.java
@@ -1,15 +1,22 @@
 package com.praktos.praktos.repository;
 
 import com.praktos.praktos.entity.Sources;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Objects;
 
 public interface SourcesRepository extends CrudRepository<Sources,Integer> {
+
     List<Sources> findSourcesByAuthor(String author);
     List<Sources> findSourcesByName(String name);
     List<Sources> findSourcesByPublisher(String publisher);
     List<Sources> findSourcesByYear(Integer year);
     List<Sources> findSourcesByTags(String tags);
+
+@Query("select distinct author from Sources ORDER BY author")
+    List<Object> getAuthorsList();
+
 }
